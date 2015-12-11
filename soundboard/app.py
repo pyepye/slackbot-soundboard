f08435cc6b64aeba1b9b52c4bb0c937268668c3b
Matt Pye <pyematt@gmail.com>
2015-12-11 11:30:05 +0000

    Use request.form data

--- soundboard/app.py
+++ soundboard/app.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-from urllib2 import unquote
 
 from flask import Flask, jsonify, request
 from fuzzywuzzy import process
@@ -11,15 +10,7 @@ app.config['SECRET_KEY'] = 'changeme'
 
 @app.route('/arnie', methods=['POST'])
 def soundboard():
-    all_data = unquote(request.data)
-    all_data = all_data.split('\n')
-    data = {}
-    for line in all_data:
-        key, value = line.split('=', 1)
-        data[key] = value
-
-    command = data['command']
-    __, sound = command.split('/arnie ')
+    sound = request.form['text']
     audio_dir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         '../audio'
@@ -38,7 +29,7 @@ def soundboard():
             'response_type': 'in_channel',
             'text': 'Could not find {0}'.format(sound),
             'attachments': [{
-                    'text': 'Your choices are {0}'.format(', '.join(files))
+                    'text': 'Choices are \'{0}\''.format(',   '.join(files))
             }]
         }
     return jsonify(response)
